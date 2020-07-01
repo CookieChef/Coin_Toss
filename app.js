22c3c3b25f166c68acc7228c91fdd1ed113546db
Sara Martinez <s_martinez_1990@outlook.com>
2020-07-01 12:55:12 -0400

    win logic created

--- app.js
+++ app.js
@@ -8,10 +8,22 @@ var losses = 0;
 
 function flipThatCoin (result) {
 
+//generate a random number and depending on the random number display an image that represents that random number.
    var randomNumber = Math.floor(Math.random() * 2);
 
    if (randomNumber === 0) {
        $("#coin-image").html ("<img src='images/coin_heads.png'/>");
-
    }
+   else {
+       $("#coin-image").html ("<img src='images/coin_tails.png'/");
+   }
+
+   //if results equals to randomNumber do the following:
+
+   if(result === randomNumber) {
+       wins++;
+       $("#win-lose").html("<h2>Winner!</h2>");
+       $("#wins").html("<h3>" + wins + "</h3>");
+   }
+
 }

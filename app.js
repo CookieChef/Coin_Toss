efe04f769bba361fdcb397fa3d5c8b5fd2352a53
Sara Martinez <s_martinez_1990@outlook.com>
2020-07-01 12:44:24 -0400

    random number for guess and some logic

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Toss a coin to your Witcher!");
+
+//variable declaration
+var headsCount = 0;
+var trailsCount = 0; 
+var wins = 0;
+var losses = 0;
+
+function flipThatCoin (result) {
+
+   var randomNumber = Math.floor(Math.random() * 2);
+
+   if (randomNumber === 0) {
+       $("#coin-image").html ("<img src='images/coin_heads.png'/>");
+
+   }
+}
